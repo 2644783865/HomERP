bf842244c3ca017974bcafb506bf8a772ab2c394
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-11 23:04:54 +0200

    Add BankAccountNumber class.

---
+++ HomERP.Domain.Tests/BankAccountNumberTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using HomERP.Domain.Helpers;
+
+namespace HomERP.Domain.Tests
+{
+    [TestClass]
+    public class BankAccountNumberTests
+    {
+        [TestMethod]
+        public void Test_ParseAccountNumber_When_IncorrectNumber_Returns_False()
+        {
+            //arrange
+            BankAccountNumber account = new BankAccountNumber();
+            //act
+            bool result = BankAccountNumber.TryParse("15114020170000460202562537", out account);
+            //assert
+            Assert.IsFalse(result);
+            Assert.AreEqual(String.Empty, account.ToString());
+        }
+
+        [TestMethod]
+        public void Test_ParseAccountNumber_When_CorrectNumber_Returns_True()
+        {
+            //arrange
+            BankAccountNumber account = new BankAccountNumber();
+            //act
+            bool result = BankAccountNumber.TryParse("15114020170000460202562536", out account);
+            //assert
+            Assert.IsTrue(result);
+            Assert.AreEqual("15 1140 2017 0000 4602 0256 2536", account.ToString());
+        }
+    }
+}
---
+++ HomERP.Domain/Helpers/BankAccountNumber.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace HomERP.Domain.Helpers
+{
+    public class BankAccountNumber
+    {
+        private string accountNumber;
+
+        public BankAccountNumber() { }
+
+        private BankAccountNumber(string number)
+        {
+            this.accountNumber = number;
+        }
+
+        public static bool TryParse(string numberstring, out BankAccountNumber accNumber)
+        {
+            //normalize
+            numberstring = Regex.Replace(numberstring, "[^0-9]", "");
+            string numberToCheck = "PL" + numberstring;
+            numberToCheck = numberToCheck.Substring(4) + numberToCheck.Substring(0, 4);
+
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            StringBuilder builder = new StringBuilder();
+            foreach (char letter in numberToCheck)
+            {
+                if(letters.Contains(letter))
+                {
+                    string value = (letters.IndexOf(letter) + 10).ToString();
+                    builder.Append(value);
+                }
+                else
+                {
+                    builder.Append(letter);
+                }
+            }
+            int checksum = 0;
+            foreach(char sign in builder.ToString())
+            {
+                checksum *= 10;
+                checksum += int.Parse(sign.ToString());
+                checksum %= 97;
+            }
+            if (checksum == 1)
+            {
+                accNumber = new BankAccountNumber(numberstring);
+            }
+            else
+            {
+                accNumber = new BankAccountNumber("");
+            }
+            return checksum == 1;
+        }
+
+        public override string ToString()
+        {
+            if (this.accountNumber.Length!=26)
+            { return String.Empty; }
+            StringBuilder formattedNumber = new StringBuilder();
+            formattedNumber.Append(this.accountNumber.Substring(0, 2));
+            for (byte i=1; i<7; i++)
+            {
+                formattedNumber.Append(" " + this.accountNumber.Substring(i * 4-2, 4));
+            }
+            return formattedNumber.ToString();
+        }
+    }
+}
