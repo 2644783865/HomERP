4458b9075abf78f41ca127a212f5693d1b39f1ae
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-15 18:53:19 +0200

    Add User entity.

---
+++ HomERP.Domain/Entity/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+using HomERP.Domain.Entity.Abstract;
+
+namespace HomERP.Domain.Entity
+{
+    public class User : IUser
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public System.Net.Mail.MailAddress Email { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
