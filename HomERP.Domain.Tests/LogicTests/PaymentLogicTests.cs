a6092e104aa00ad820ffd424e66d4a8414ba2768
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-22 22:36:59 +0200

    Add Payment Provider, testing Moq usage.

---
+++ HomERP.Domain.Tests/LogicTests/PaymentLogicTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Moq;
+
+using HomERP.Domain.Entity;
+using HomERP.Domain.Repository.Abstract;
+using HomERP.Domain.Logic;
+
+namespace HomERP.Domain.Tests.LogicTests
+{
+    [TestClass]
+    public class PaymentLogicTests
+    {
+        private Payment PrepareExamplePayment()
+        {
+            return new Payment
+            {
+                Amount = 102,
+                Direction = Helpers.CashFlowDirection.Increase,
+                Time = new DateTime(2017, 1, 1, 10, 0, 0)
+            };
+        }
+        [TestMethod]
+        public void Test_SavePayment()
+        {
+            Payment payment = PrepareExamplePayment();
+            Mock<IPaymentRepository> mock = new Mock<IPaymentRepository>();
+            mock.Setup(m => m.Payments).Returns(new Payment[]
+            {
+                new Payment { Amount=100, Direction= Helpers.CashFlowDirection.Decrease},
+                new Payment { Amount=65.02m, Direction= Helpers.CashFlowDirection.Decrease}
+            }
+                );
+            PaymentProvider provider = new PaymentProvider(mock.Object);
+            //act
+            provider.SavePayment(payment);
+            //assert
+            mock.Verify(m => m.SavePayment(payment));
+        }
+    }
+}
---
+++ HomERP.Domain/Logic/PaymentProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+using HomERP.Domain.Entity;
+using HomERP.Domain.Repository.Abstract;
+using HomERP.Domain.Logic.Abstract;
+
+namespace HomERP.Domain.Logic
+{
+    public class PaymentProvider: IPaymentProvider
+    {
+        IPaymentRepository paymentRepository;
+
+        public PaymentProvider(IPaymentRepository repository)
+        {
+            this.paymentRepository = repository;
+        }
+
+        public IEnumerable<Payment> Payments
+        { get { return paymentRepository.Payments; } }
+
+        public Payment DeletePayment(int paymentId)
+        {
+            return paymentRepository.DeletePayment(paymentId);
+        }
+
+        public void SavePayment(Payment payment)
+        {
+            paymentRepository.SavePayment(payment);
+        }
+    }
+}
