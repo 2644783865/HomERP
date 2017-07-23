d98d9b8aefaae01cd0545263eab07571d5688cbe
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-23 15:30:01 +0200

    Complete tests for Payment logic.

--- HomERP.Domain.Tests/LogicTests/PaymentLogicTests.cs
+++ HomERP.Domain.Tests/LogicTests/PaymentLogicTests.cs
@@ -24,6 +24,25 @@ namespace HomERP.Domain.Tests.LogicTests
                 Time = new DateTime(2017, 1, 1, 10, 0, 0)
             };
         }
+
+        [TestMethod]
+        public void Test_GetPayments()
+        {
+            //arrange
+            Mock<IPaymentRepository> mock = new Mock<IPaymentRepository>();
+            mock.Setup(m => m.Payments).Returns(new Payment[]
+            {
+                new Payment { Amount=100, Direction= Helpers.CashFlowDirection.Decrease},
+                new Payment { Amount=65.02m, Direction= Helpers.CashFlowDirection.Decrease}
+            }
+                );
+            PaymentProvider provider = new PaymentProvider(mock.Object);
+            //act
+            IEnumerable<Payment> Payments = provider.Payments;
+            //assert
+            Payments.Should().HaveCount(2, "you have 2 entities in the repository");
+        }
+
         [TestMethod]
         public void Test_SavePayment()
         {
@@ -39,7 +58,32 @@ namespace HomERP.Domain.Tests.LogicTests
             //act
             provider.SavePayment(payment);
             //assert
+            //In this place we have to focus on that underlying repository method has been properly called
+            //instead of wandering if entity has been properly saved - this is the repository responsibility.
             mock.Verify(m => m.SavePayment(payment));
+
+        }
+
+        [TestMethod]
+        public void Test_DeletePayment()
+        {
+            //arrange
+            Mock<IPaymentRepository> mock = new Mock<IPaymentRepository>();
+            mock.Setup(m => m.Payments).Returns(new Payment[]
+            {
+                new Payment { Id = 1, Amount=100, Direction= Helpers.CashFlowDirection.Decrease},
+                new Payment { Id = 2, Amount=65.02m, Direction= Helpers.CashFlowDirection.Decrease}
+            }
+                );
+            Payment paymentToDelete = mock.Object.Payments.Where(p=>p.Id==2).First();
+            PaymentProvider provider = new PaymentProvider(mock.Object);
+            //act
+
+            provider.DeletePayment(paymentToDelete.Id);
+            //assert if repository delete method has been called with proper identifier
+            
+            mock.Verify(m => m.DeletePayment(2));
+
         }
     }
 }
