c1709226e3cc7c23115d850f01beba117047a872
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-26 15:48:37 +0200

    Add test - save records to database.

--- HomERP.Integration.Tests/MSSQLServer/CommunicationTests.cs
+++ HomERP.Integration.Tests/MSSQLServer/CommunicationTests.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using FluentAssertions;
 
 using HomERP.Domain.Repository.EntityFramework;
+using HomERP.Domain.Entity;
 
 namespace HomERP.Integration.Tests.MSSQLServer
 {
@@ -27,6 +28,30 @@ namespace HomERP.Integration.Tests.MSSQLServer
             count = context.Payments.Count();
             //assert
             count.Should().BeGreaterOrEqualTo(0);
+        }
+
+        [Ignore]
+        [TestMethod]
+        public void AddRecordsToMSSQLServer()
+        {
+            //arrange
+            var builder = new DbContextOptionsBuilder<EfDbContext>()
+                .UseSqlServer("Server=localhost;Database=" + databaseName + "; Trusted_Connection=True;");
+            EfDbContext context = new EfDbContext(builder.Options);
+
+            User user = new User { Email = "marcin@homerp.pl", Name = "Marcin" };
+            Account account = new Account { InitialAmount = 10, Name = "Portfel" };
+            Payment payment = new Payment
+            {
+                Account = account,
+                Amount = 100,
+                Direction = Domain.Helpers.CashFlowDirection.Increase,
+                Time = DateTime.Now,
+                User = user
+            };
+
+            context.Payments.Add(payment);
+            context.SaveChanges();
         }
     }
 }
