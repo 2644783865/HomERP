d60d81564ef860515ea36dbf37129da63d72c83f
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-28 19:08:26 +0200

    Add sample field names with annotations.

--- HomERP.Domain/Entity/Payment.cs
+++ HomERP.Domain/Entity/Payment.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 using HomERP.Domain.Entity.Abstract;
 using HomERP.Domain.Helpers;
@@ -12,7 +13,9 @@ namespace HomERP.Domain.Entity
     public class Payment : IOperation
     {
         public int Id { get; set; }
+        [Display(Name = "Konto")]
         public Account Account { get; set; }
+        [Display(Name = "Kwota")]
         public decimal Amount { get; set; }
         public CashFlowDirection Direction { get; set; }
         public DateTime Time { get; set; }
