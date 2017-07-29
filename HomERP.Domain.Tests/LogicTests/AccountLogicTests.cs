26330d989e6bc70deb5e6b31574d55c70a7f59b8
Marcin Szczygieł <marcin@szczygiel.info>
2017-07-29 20:14:18 +0200

    Add AccountProvider and UserProvider.

---
+++ HomERP.Domain.Tests/LogicTests/AccountLogicTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using Moq;
+
+using HomERP.Domain.Entity;
+using HomERP.Domain.Repository.Abstract;
+using HomERP.Domain.Logic.Abstract;
+using HomERP.Domain.Logic;
+
+namespace HomERP.Domain.Tests.LogicTests
+{
+    [TestClass]
+    public class AccountLogicTests
+    {
+        private Account PrepareExampleAccount()
+        {
+            return new Account
+            {
+                InitialAmount = 50,
+                Name = "Portfel"
+            };
+        }
+
+        [TestMethod]
+        public void Test_GetAccounts()
+        {
+            //arrange
+            Mock<IAccountRepository> mock = new Mock<IAccountRepository>();
+            mock.Setup(m => m.Accounts).Returns(new Account[]
+            {
+                new Account { InitialAmount=100, Name = "Konto1"},
+                new Account { InitialAmount=65.02m, Name = "Konto2"}
+            }
+                );
+            AccountProvider provider = new AccountProvider(mock.Object);
+
+            //act
+            IEnumerable<Account> Accounts = provider.Accounts;
+
+            //assert
+            Accounts.Should().HaveCount(2, "you have 2 entities in the repository");
+        }
+
+        [TestMethod]
+        public void Test_SaveAccount()
+        {
+            //arrange
+            Account account = PrepareExampleAccount();
+            Mock<IAccountRepository> mock = new Mock<IAccountRepository>();
+            mock.Setup(m => m.Accounts).Returns(new Account[]
+            {
+                new Account { InitialAmount=100, Name = "Konto1"},
+                new Account { InitialAmount=65.02m, Name = "Konto2"}
+            }
+                );
+            AccountProvider provider = new AccountProvider(mock.Object);
+
+            //act
+            provider.SaveAccount(account);
+
+            //assert
+            //In this place we have to focus on that underlying repository method has been properly called
+            //instead of wandering if entity has been properly saved - this is the repository responsibility.
+            mock.Verify(m => m.SaveAccount(account));
+        }
+
+        [TestMethod]
+        public void Test_DeleteAccount()
+        {
+            //arrange
+            Mock<IAccountRepository> mock = new Mock<IAccountRepository>();
+            mock.Setup(m => m.Accounts).Returns(new Account[]
+            {
+                new Account { Id = 1, InitialAmount=100, Name = "Konto1"},
+                new Account { Id = 2, InitialAmount=65.02m, Name = "Konto2"}
+            }
+                );
+            Account accountToDelete = mock.Object.Accounts.Where(p => p.Id == 2).First();
+            AccountProvider provider = new AccountProvider(mock.Object);
+
+            //act
+            provider.DeleteAccount(accountToDelete.Id);
+
+            //assert if repository delete method has been called with proper identifier
+            mock.Verify(m => m.DeleteAccount(2));
+        }
+    }
+}
---
+++ HomERP.Domain/Logic/UserProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HomERP.Domain.Entity;
+using HomERP.Domain.Logic.Abstract;
+using HomERP.Domain.Repository.Abstract;
+
+namespace HomERP.Domain.Logic
+{
+    public class UserProvider : IUserProvider
+    {
+        private IUserRepository repository;
+
+        public UserProvider(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IEnumerable<User> Users
+        { get { return repository.Users; } }
+
+        public User DeleteUser(int userId)
+        {
+            return repository.DeleteUser(userId);
+        }
+
+        public void SaveUser(User user)
+        {
+            repository.SaveUser(user);
+        }
+    }
+}
