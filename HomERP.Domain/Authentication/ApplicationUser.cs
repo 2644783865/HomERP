cb8c1cab2bbb3d6c5a9c5db957e16d44fe7fbbd6
Marcin Szczygieł <marcin@szczygiel.info>
2017-08-12 19:20:12 +0200

    Enhance family summary display.

--- HomERP.Domain/Authentication/ApplicationUser.cs
+++ HomERP.Domain/Authentication/ApplicationUser.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using HomERP.Domain.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomERP.Domain.Authentication
 {
     public class ApplicationUser : IdentityUser
     {
+        /// <summary>
+        /// Overriden from base class in order to localize annotations.
+        /// </summary>
+        [Display(Name ="Nazwa użytkownika")]
+        public override string UserName { get; set; }
+
         public Family Family { get; set; }
     }
 }
