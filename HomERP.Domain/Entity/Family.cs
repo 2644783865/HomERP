0fba3cf87955a5e6eaae5556c826a4fca3cfba6e
Marcin Szczygieł <marcin@szczygiel.info>
2017-09-04 22:14:04 +0200

    Add display name and constructor to Family entity.

--- HomERP.Domain/Entity/Family.cs
+++ HomERP.Domain/Entity/Family.cs
@@ -12,9 +12,16 @@ namespace HomERP.Domain.Entity
         public int Id { get; set; }
         [Required]
         [StringLength(100, MinimumLength = 3, ErrorMessage = "Nazwa musi mieć od {0} do {1} znaków.")]
+        [Display(Name = "Nazwa rodziny")]
         public string Name { get; set; }
         [StringLength(200, ErrorMessage = "Opis może mieć max. {0} znaków.")]
+        [Display(Name = "Opis")]
         public string Description { get; set; }
         public virtual ICollection<ApplicationUser> FamilyMembers { get; }
+
+        public Family()
+        {
+            FamilyMembers = new List<ApplicationUser>();
+        }
     }
 }
