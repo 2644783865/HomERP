96255a1730f1288381ead87004605926676e758c
Marcin Szczygieł <marcin@szczygiel.info>
2017-09-11 22:26:06 +0200

    Test SaveFamily method of FamilyProvider.

--- HomERP.Domain.Tests/LogicTests/FamilyProviderTests.cs
+++ HomERP.Domain.Tests/LogicTests/FamilyProviderTests.cs
@@ -42,7 +42,7 @@ namespace HomERP.Domain.Tests.LogicTests
         }
 
         [TestMethod]
-        public void Should_Call_FamilyRepository_SaveFamily()
+        public void Should_Call_FamilyRepository_SaveFamily_When_User_Saves_His_Own_Family()
         {
             //arrange
             Family Family = PrepareExampleFamily();
@@ -59,6 +59,88 @@ namespace HomERP.Domain.Tests.LogicTests
             //instead of wandering if entity has been properly saved - this is the repository responsibility.
             mock.Verify(m => m.SaveFamily(Family));
             result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void Should_Call_FamilyRepository_SaveFamily_When_User_With_No_Family_Yet_Saves_New_Family()
+        {
+            //arrange
+            Family Family = PrepareExampleFamily();
+            Family.Id = 0;
+            Mock<IFamilyRepository> mock = this.PrepareMockFamilyRepository();
+            FamilyProvider provider = new FamilyProvider(mock.Object);
+            Mock<IApplicationUser> mockUser = new Mock<IApplicationUser>();
+            mockUser.Setup(u => u.FamilyId).Returns((int?)null);
+
+            //act
+            bool result = provider.SaveFamily(Family, mockUser.Object);
+
+            //assert
+            //In this place we have to focus on that underlying repository method has been properly called
+            //instead of wandering if entity has been properly saved - this is the repository responsibility.
+            mock.Verify(m => m.SaveFamily(Family));
+            result.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ShouldNot_Call_FamilyRepository_Trying_To_Save_Foreign_Family()
+        {
+            //arrange
+            Family Family = PrepareExampleFamily();
+            Mock<IFamilyRepository> mock = this.PrepareMockFamilyRepository();
+            FamilyProvider provider = new FamilyProvider(mock.Object);
+            Mock<IApplicationUser> mockUser = new Mock<IApplicationUser>();
+            mockUser.Setup(u => u.FamilyId).Returns(2);
+
+            //act
+            bool result = provider.SaveFamily(Family, mockUser.Object);
+
+            //assert
+            //In this place we have to focus on that underlying repository method has been properly called
+            //instead of wandering if entity has been properly saved - this is the repository responsibility.
+            mock.Verify(m => m.SaveFamily(Family), Times.Never);
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ShouldNot_Call_FamilyRepository_Trying_To_Save_Foreign_Family_And_User_Has_No_Family()
+        {
+            //arrange
+            Family Family = PrepareExampleFamily();
+            Mock<IFamilyRepository> mock = this.PrepareMockFamilyRepository();
+            FamilyProvider provider = new FamilyProvider(mock.Object);
+            Mock<IApplicationUser> mockUser = new Mock<IApplicationUser>();
+            mockUser.Setup(u => u.FamilyId).Returns((int?)null);
+
+            //act
+            bool result = provider.SaveFamily(Family, mockUser.Object);
+
+            //assert
+            //In this place we have to focus on that underlying repository method has been properly called
+            //instead of wandering if entity has been properly saved - this is the repository responsibility.
+            mock.Verify(m => m.SaveFamily(Family), Times.Never);
+            result.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void ShouldNot_Call_FamilyRepository_Trying_To_Save_New_Family_And_User_Already_Has_Family()
+        {
+            //arrange
+            Family Family = PrepareExampleFamily();
+            Family.Id = 0;
+            Mock<IFamilyRepository> mock = this.PrepareMockFamilyRepository();
+            FamilyProvider provider = new FamilyProvider(mock.Object);
+            Mock<IApplicationUser> mockUser = new Mock<IApplicationUser>();
+            mockUser.Setup(u => u.FamilyId).Returns(1);
+
+            //act
+            bool result = provider.SaveFamily(Family, mockUser.Object);
+
+            //assert
+            //In this place we have to focus on that underlying repository method has been properly called
+            //instead of wandering if entity has been properly saved - this is the repository responsibility.
+            mock.Verify(m => m.SaveFamily(Family), Times.Never);
+            result.Should().BeFalse();
         }
 
         [TestMethod]
